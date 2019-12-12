0f02b4afb90832e4abf521790ea70a04267817c5
Jiří Setnička <setnicka@seznam.cz>
2019-12-12 15:18:00 +0100

    Added setup.py and requirements.txt
    Also uploaded to pypi as https://pypi.org/project/ulozto-downloader/
    
    Closes #1

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+import setuptools  # noqa
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name='ulozto_downloader',
+  version='1.2',
+  license='MIT',
+  description='Uloz.to quick multiple sessions downloader.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author='Jiří Setnička',
+  author_email='setnicka@seznam.cz',
+  url='https://github.com/setnicka/ulozto_downloader',
+  download_url='https://github.com/setnicka/ulozto_downloader/archive/1.1.tar.gz',
+  install_requires=[
+          'requests',
+          'Pillow',
+  ],
+  python_requires='>=3.5',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
