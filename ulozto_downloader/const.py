a48b5c9a5f8fb1eef662e4ba36d4389afa47d64f
vlado <vla@do>
2021-04-27 23:47:37 +0200

    Better msgs for direct links

--- ulozto_downloader/const.py
+++ ulozto_downloader/const.py
@@ -1,2 +1,6 @@
 CLI_STATUS_STARTLINE = 6
-XML_HEADERS = {"X-Requested-With": "XMLHttpRequest"}
+XML_HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    # "referer": "https://www.google.com"
+}
