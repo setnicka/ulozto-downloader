d5060a1d9ab2d086ad2c90bfe8b21caa07a7dc16
vlado <vla@do>
2021-04-29 14:38:27 +0200

    Backport changes from uld

--- ulozto_downloader/const.py
+++ ulozto_downloader/const.py
@@ -1,6 +1,6 @@
 CLI_STATUS_STARTLINE = 6
 XML_HEADERS = {
+    "Accept-Encoding": "gzip",
     "X-Requested-With": "XMLHttpRequest",
-    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    # "referer": "https://www.google.com"
+    "User-Agent": "Go-http-client/1.1",
 }
