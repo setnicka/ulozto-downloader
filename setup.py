6747b3fef444a67cb600c33db1e0ab9f8644c2eb
zbyna <zbyna@users.noreply.github.com>
2022-07-11 02:12:25 +0200

    Proper ut8 handling for install from github in win

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools  # noqa
 
 from uldlib import __version__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r",encoding='utf8') as fh:
     long_description = fh.read()
 
 setup(
