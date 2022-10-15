b53ad696fc54c18ec1ee41dfdcd95f1d331419bc
Marek Vigas <vigas147@gmail.com>
2022-10-15 17:14:22 +0200

    refactor variable names and add types

--- uldlib/cmd.py
+++ uldlib/cmd.py
@@ -34,7 +34,7 @@ def run():
     args = parser.parse_args()
 
     # TODO: implement other frontends and allow to choose from them
-    frontend = ConsoleFrontend(args)
+    frontend = ConsoleFrontend(show_parts=args.parts_progress)
 
     tflite_available = importlib.util.find_spec('tflite_runtime')
     tkinter_available = importlib.util.find_spec('tkinter')
