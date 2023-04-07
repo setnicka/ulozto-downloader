1bc450f93e6acef7018571b175dbaa76df04acc5
Lukas Bacovsky <l.bacovsky@gmail.com>
2023-04-07 16:59:34 +0200

    fixed "Unresolved reference 'r'"

--- uldlib/page.py
+++ uldlib/page.py
@@ -14,8 +14,6 @@ from uldlib.torrunner import TorRunner
 from .const import XML_HEADERS, DEFAULT_CONN_TIMEOUT
 from .linkcache import LinkCache
 
-from requests.sessions import RequestsCookieJar
-
 
 def parse_single(text, regex):
     p = re.compile(regex, re.IGNORECASE)
@@ -113,8 +111,8 @@ class Page:
         # If file is file-tracking link we need to get normal file link from it
         if url.startswith('{uri.scheme}://{uri.netloc}/file-tracking/'.format(uri=parsed_url)):
             s.get(url, allow_redirects=False)
-            if 'Location' in r.headers:
-                self.url = strip_tracking_info(r.headers['Location'])
+            if 'Location' in s.headers:
+                self.url = strip_tracking_info(s.headers['Location'])
                 parsed_url = urlparse(self.url)
 
         r = s.get(self.url)
